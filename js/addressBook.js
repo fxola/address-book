de02003a43947c457708825c1678495b05be5f4c
Fola62 <Afolabiabass62@gmail.com>
2018-04-03 15:26:48 +0100

    updated css

--- js/addressBook.js
+++ js/addressBook.js
@@ -2,45 +2,56 @@ var submitButton = document.getElementById('submit');
 var contactName = document.getElementById('name');
 var email = document.getElementById('email');
 var phone = document.getElementById('phone');
-var picture = document.getElementById('image');
 var output = document.getElementById('form-output');
 var listOfNames = '';
 var addressBook = [];
 var parent = document.getElementById('list-output');
 // var deleteUser = document.getElementById('delete');
 
-
-
 submitButton.addEventListener('click',function(e){
+
     e.preventDefault();
   
     var contact = {
         name: contactName.value, 
         emailAddress: email.value,
         phoneNumber : phone.value,
-        displayPicture : picture.value
     }
 
-    addressBook.push(contact);    
+    addressBook.push(contact);  
+
     sessionStorage.setItem('contact', JSON.stringify(addressBook));
 
-    addressBook.forEach(function(element, index, array){
-        listOfNames = "<li><a href='editbook.html'>" + element.name + "  </a> <button class='deleteContact' data-id = "+index+">DELETE</button></li>";
+    var contactDetails = JSON.parse(sessionStorage.contact);
+    contactDetails.forEach(function(element, index, array){
+        listOfNames = "<li class ='contact-name'><a href='editbook.html?name="+element.name+"'>" + element.name + "  </a> <button class='deleteContact' data-id = "+index+">DELETE</button></li>";
     });
-    
+
     parent.innerHTML += listOfNames;
 
-  function clear(){
-    contactName.value ='';
-}
-clear();
+    function clear(){
+        contactName.value ='';
+    }
 
+    clear();
 });
 
 parent.addEventListener('click', function(e){
     if(e.target.classList.contains('deleteContact')){
-        console.log('hey');}
-    // console.log('hey');
+        parent.innerHTML = "";
+        
+        var contactID = e.target.getAttribute('data-id');
+        var contactDetails = JSON.parse(sessionStorage.contact);
+       var contactLeft = contactDetails.splice(contactID,1);
+       contactLeft.forEach(function(element, index, array){
+            listOfNames = "<li><a href='editbook.html'>" + element.name + "  </a> <button class='deleteContact' data-id = "+index+">DELETE</button></li>";
+        });
+    parent.innerHTML += listOfNames;
+       
+    }
 });
+
+    // console.log('hey');
+
 
   
